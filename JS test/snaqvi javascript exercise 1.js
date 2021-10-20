12455f9fa1e6d91da75fb50132446ad8d1e8aae0
san5281 <san5281@psu.edu>
2021-10-20 16:00:22 -0400

    testing ?

---
+++ JS test/snaqvi javascript exercise 1.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded',test,false);
+
+function nuttob() {
+     var buttons = document.getElementsByTagName("Nissan")
+     buttons[0].addEventListener('click', colorChange, false)
+     buttons[1].addEventListener('click', test, false)
+}
+function chameleon(){
+    var camoflague = document.getElementById("colorToggle")
+    camoflague.style.backgroundColor = "white"
+}
+function test(){
+while (buttons = true){
+alert("Link to About Page")    
+}    
+}
+ <script type="text/javascript" src="showHide.js">/**/</script>
